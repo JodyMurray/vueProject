feffd4cedb208b2b930133ce178fa1e9ed017840
Jody M <jodyamm@hotmail.com>
2023-10-25 06:52:38 +0000

    Add: Vue CDN and homepage component

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Home Component</h1>
+    <p>This is the homepage of your Vue.js application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  margin: 20px;
+}
+</style>
