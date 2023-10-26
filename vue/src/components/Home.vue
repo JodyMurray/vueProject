366f0b23f528fc98811b0c525e2ed66daf284897
Jody M <jodyamm@hotmail.com>
2023-10-26 06:43:48 +0000

    Add: Home screen image and match navbar to theme

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <Navbar />
-    <div class="content">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the content of your home page.</p>
+    <div class="overlay">
+      <div class="content">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the content of your home page.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -21,13 +23,32 @@ export default {
 
 <style>
 .home {
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-image: url('src/assets/recipehome.jpeg'); 
+  background-size: cover;
+  background-position: center;
+  height: 100vh; 
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7); 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
 }
 
 .content {
-  margin-top: 60px;
   text-align: center;
+  color: white;
 }
+
 </style>
--- vue/src/components/Navbar.vue
+++ vue/src/components/Navbar.vue
@@ -1,11 +1,9 @@
 <template>
   <nav class="navbar">
-    <img class="logo" src="/./././src/assets/logo1.png" alt="Logo" />
+    <img class="logo" src="../assets/logo1.png" alt="Logo" />
     <ul>
       <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="#">Sign In</a></li>
     </ul>
   </nav>
 </template>
@@ -18,17 +16,26 @@ export default {
 
 <style>
 .logo {
-  height: 40px;
-  margin-top: 13px;
+  height: 35px;
+  margin-left: 15px;
+  margin-top: 8px;
 }
 .navbar {
-  background-color: #ff7400;
+  background-color: rgba(255, 255, 255, 0.1); /* Adjust the background color and opacity */
+  backdrop-filter: blur(10px); /* Add a backdrop filter for a frosted glass effect */
   overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  border-top: 1px solid rgba(255, 255, 255, 0.1); /* Add a border on top for separation */
+  z-index: 2;
 }
 
 .navbar ul {
   list-style-type: none;
-  margin: 0;
+  margin-top: 5px;
   padding: 0;
   display: flex;
 }
@@ -40,10 +47,12 @@ export default {
 .navbar a {
   color: white;
   text-decoration: none;
+  font-weight: bold; /* Make the text more prominent */
 }
 
 /* Styles for the hover effect */
 .navbar a:hover {
   text-decoration: underline;
+  color: #ff7400; /* Change the text color on hover to a contrasting color */
 }
 </style>
